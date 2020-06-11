75d74742e37ad9f95500c87df1dd69fd2a2c7a80
Dimas-Pratama-Purwadinata <54875915+Dimas-Pratama-Purwadinata@users.noreply.github.com>
2020-06-11 21:12:56 +0700

    Update update_customer_details.php

--- update_customer_details.php
+++ update_customer_details.php
@@ -35,7 +35,7 @@ include_once("init.php");
 </div>
 <div id="content">
     <div class="page-full-width cf">
-        <div class="side-menu fl">
+        <div class="side-menu fr">
             <h3>Customers Management</h3>
             <ul>
                 <li><a href="add_customer.php">Add Customer</a></li>
