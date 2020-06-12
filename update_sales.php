d3c8e022f3683f8d7e1341717a2c8ed6de586ed8
nadhifrl <55243634+nadhifrl@users.noreply.github.com>
2020-06-12 13:48:02 +0700

    Update update_sales.php

--- update_sales.php
+++ update_sales.php
@@ -362,6 +362,7 @@ include_once("init.php");
                     </div>
                 </div>
             </div>
+        </div>
         <?php include_once("tpl/footer.php"); ?>
     </body>
 </html>
